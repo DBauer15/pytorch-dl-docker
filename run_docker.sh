dc52098b80835b8b9de6d431e8895384271bd183
David Bauer <DBauer15@users.noreply.github.com>
2022-02-15 11:19:37 -0800

    Update run_docker.sh

--- run_docker.sh
+++ run_docker.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-
 docker run \
     --gpus all \
     -it \
     --rm \
+    --ipc=host \
     --user `id -u`:`id -g` \
-    -w /workspace \
+    -v `pwd`/training:/workspace/training \
+    -v /media/data/mlready:/media/data/mlready \
+    -w /workspace/training \
     --expose 6006 \
+    -p 6006:6006 \
     "${@:2}" \
-    $1:latest
+    $1
